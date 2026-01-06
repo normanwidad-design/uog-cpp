502f2f1fbb12eb4a398537c0dc8c58870d91e8a2
Cafesua <khiem.nguyen@glasgow.ac.uk>
2026-01-06 05:09:02 +0000

    Create chapter-00 and files inside it

---
+++ chap-00/cpp/addition.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // to use std::cout
+
+// We can do some mathematics.
+int main() {
+    double a = 5.5;     
+    double b = 4.5;
+    std::cout << "a + b = " << a + b << std::endl;
+    std::cout << "This program is almost useless\n";
+    return 0;
+}
---
+++ chap-00/cpp/hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream> // to use std::cout
+
+int main() {    // Every program has function main()
+    std::cout << "Welcome everybody to ENG3091!" << std::endl;  // note semicolor at the end
+    std::cout << "\nWe shall learn:\n";                         // most of statement ends with semicolon
+    std::cout << "(1) C++ Basics\n";
+    std::cout << "(2) Object-oriented in C++\n";
+    std::cout << "(3) Some standard libraries in C++\n";
+    std::cout << "(4) Unified Modelling Language\n";
+    return 0;           // The program returns "0", meaning it executes successfully.
+}
