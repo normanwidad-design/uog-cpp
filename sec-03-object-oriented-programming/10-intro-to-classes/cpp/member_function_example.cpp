5888e17f39e3182c8274144ea49da4f833c70173
Cafe Sua <khiem.nguyen@glasgow.ac.uk>
2026-02-22 04:30:20 +0000

    Create Chapter 10 in Section 03

---
+++ sec-03-object-oriented-programming/10-intro-to-classes/cpp/member_function_example.cpp
@@ -0,0 +1,23 @@
+// Member function version
+#include <iostream>
+
+struct Date
+{
+    int year {};        // we don't use m_ here for struct.
+    int month {};
+    int day {};
+
+    void print() // defines a member function named print
+    {
+        std::cout << year << '/' << month << '/' << day;
+    }
+};
+
+int main()
+{
+    Date today { 2020, 10, 14 }; // aggregate initialize our struct
+    today.day = 16; // member variables accessed using member selection operator (.)
+    today.print();  // member functions also accessed using member selection operator (.)
+    std::cout << '\n';
+    return 0;
+}
