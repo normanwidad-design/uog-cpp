f77ce19ab5d7ccea285dbca99be7001e5fd1a15c
Cafe Sua <khiem.nguyen@glasgow.ac.uk>
2026-02-25 23:57:34 +0000

    Update Chapter 11 in Section 03

--- sec-03-object-oriented-programming/11-classes/cpp/class_private_by_default.cpp
+++ sec-03-object-oriented-programming/11-classes/cpp/class_private_by_default.cpp
@@ -16,14 +16,15 @@ class Date // now a class instead of a struct
 
 int main()  // main is a part of public
 {
-    Date today { 2020, 10, 14 }; // compile error: can no longer use aggregate initialization
+    // Date today { 2020, 10, 14 }; // compile error: can no longer use aggregate initialization
 
     // private members can not be accessed by the public
     // You cannot do something silly like this.
+    /*
     today.m_day = 42;   // compile error: the m_day member is private. 
     today.m_month = 42; // compile error: the m_month member is private.
     today.m_year = 42;  // compile error: the m_year member is private.
     today.print();      // compile error: the print() member function is private.
-
+    */
     return 0;
 }
