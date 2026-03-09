7edab0dc0c27dfe190bd4fce4fc140d1dbf7bef8
Cafe Sua <khiem.nguyen@glasgow.ac.uk>
2026-03-09 03:58:49 +0000

    Update Chapter 12 in Sec 03

--- sec-03-oop/12-classes-more/cpp/method_chaining_example.cpp
+++ sec-03-oop/12-classes-more/cpp/method_chaining_example.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
-class Calc
+class Calculator
 {
 private:
     double value{};
 
 public:
-    Calc(double value) : value {value} {}
-    Calc& add(double value) { this->value += value; return *this; }
-    Calc& sub(double value) { this->value -= value; return *this; }
-    Calc& mult(double value) { this->value *= value; return *this; }
+    Calculator(double value) : value {value} {}
+    Calculator& add(double value) { this->value += value; return *this; }
+    Calculator& sub(double value) { this->value -= value; return *this; }
+    Calculator& mult(double value) { this->value *= value; return *this; }
 
     int get_value() const { return value; }
 };
 
 int main()
 {
-    Calc calc {42};
+    Calculator calc {42};
     calc.add(5).sub(3).mult(4); // method chaining
 
     std::cout << calc.get_value() << '\n';
